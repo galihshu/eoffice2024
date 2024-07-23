6efb078f7af35c32f7e73dd550131c800333cc57
randaru09 <ndaruanggoro123@gmail.com>
2024-07-23 12:19:22 +0700

    feat: add user datatable controller

---
+++ app/Http/Controllers/DataTables/UserDataTableController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\DataTables;
+
+use App\DataTables\UsersDataTable;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserDataTableController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    public function index(UsersDataTable $dataTable)
+    {
+        
+        return $dataTable->render('datatables.users.index');
+    }
+}
