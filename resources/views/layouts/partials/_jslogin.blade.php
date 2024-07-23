7f6566fdb61240d08c692609e12429d8f343250b
galihshu <galihshu@gmail.com>
2024-07-24 05:48:56 +0700

    js login

---
+++ resources/views/layouts/partials/_jslogin.blade.php
@@ -0,0 +1,11 @@
+    <!-- Swiper JS -->
+    <script src="{{ asset('eofficeadmin/libs/swiper/swiper-bundle.min.js') }}"></script>
+
+    <!-- Internal Sing-Up JS -->
+    <script src="{{ asset('eofficeadmin/js/authentication.js') }}"></script>
+
+    <!-- Show Password JS -->
+    <script src="{{ asset('eofficeadmin/js/show-password.js') }}"></script>
+
+    <!-- Auth Custom JS -->
+    <script src="{{ asset('eofficeadmin/js/auth-custom.js') }}"></script>
