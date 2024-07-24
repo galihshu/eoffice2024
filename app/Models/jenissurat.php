d89b57d45832f8297697bbb2f465ab6deeab2287
randaru09 <ndaruanggoro123@gmail.com>
2024-07-24 15:00:16 +0700

    fix: change class name and file name to PascalCase

--- app/Models/jenissurat.php
+++ app/Models/jenissurat.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class jenissurat extends Model
+class JenisSurat extends Model
 {
     use HasFactory;
-
+    protected $table = 'jenissurat';
     protected $fillable = [
         'jenis_surat',
     ];
