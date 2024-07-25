857664f908af8ac5ba4c43d0210da633ca3ab38f
galihshu <galihshu@gmail.com>
2024-07-25 11:55:50 +0700

    role and permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleSeeder::class,
             UserSeeder::class,
-            UserFactorySeeder::class,
+            // UserFactorySeeder::class,
             JenissuratSeeder::class,
         ]);
     }
