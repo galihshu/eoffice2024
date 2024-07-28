092b9bf31027579551d139b548a4e87d5da329a5
galihshu <galihshu@gmail.com>
2024-07-28 22:52:08 +0700

    logo toggle

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -5,13 +5,13 @@
         <!-- Start::header-element -->
         <div class="header-element">
           <div class="horizontal-logo">
-            <a href="index.html" class="header-logo">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/desktop-logo.png') }}" alt="logo" class="desktop-logo">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-logo.png') }}" alt="logo" class="toggle-logo">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/desktop-dark.png') }}" alt="logo" class="desktop-dark">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-dark.png') }}" alt="logo" class="toggle-dark">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/desktop-white.png') }}" alt="logo" class="desktop-white">
-              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-white.png') }}" alt="logo" class="toggle-white">
+            <a href="{{ route('home') }}" class="header-logo">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/logo-eoffice.png') }}" alt="logo" class="desktop-logo">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-logo-eoffice.png') }}" alt="logo" class="toggle-logo">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/logo-eoffice-dark.png') }}" alt="logo" class="desktop-dark">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-logo.png') }}" alt="logo" class="toggle-dark">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/logo-eoffice.png') }}" alt="logo" class="desktop-white">
+              <img src="{{ asset('eofficeadmin/images/brand-logos/toggle-logo-eoffice.png') }}" alt="logo" class="toggle-white">
             </a>
           </div>
         </div>
