480bce7e4d0365d11a6b55ae6cd13bcd5c633c66
galihshu <galihshu@gmail.com>
2024-08-06 17:21:46 +0700

    ui disposisi

--- resources/views/modules/monitoring_disposisi/show.blade.php
+++ resources/views/modules/monitoring_disposisi/show.blade.php
@@ -3,12 +3,13 @@
 @section('title', 'Tracking Disposisi')
 
 @push('styles')
-    <style>
-        .dt-length {
-            display: none;
-        }
-    </style>
 @endpush
+
+@section('content')
+@php
+    use Carbon\Carbon;
+    Carbon::setLocale('id');
+@endphp
 
 @section('content')
     <div class="block justify-between page-header md:flex">
@@ -20,7 +21,7 @@
         <ol class="flex items-center whitespace-nowrap min-w-0">
             <li class="text-[0.813rem] ps-[0.5rem]">
                 <a class="flex items-center text-primary hover:text-primary dark:text-primary truncate" href="/home">
-                    Home
+                    Data
                     <i
                         class="ti ti-chevrons-right flex-shrink-0 text-[#8c9097] dark:text-white/50 px-[0.5rem] overflow-visible rtl:rotate-180"></i>
                 </a>
@@ -31,68 +32,104 @@
             </li>
         </ol>
     </div>
+
     <div class="grid grid-cols-12 gap-6">
         <div class="xl:col-span-12 col-span-12">
             <div class="box custom-box">
                 <div class="box-header justify-between">
                     <div class="box-title">
-                        Tracking Disposisi
+                        Nomor Surat Masuk: {{ $no_surat }}
                     </div>
                 </div>
-                <div class="box-body">
-                    <div class="space-y-6 border-l-2 border-dashed">
-                        @foreach ($data as $item)
-                            <div>
-                                <div class="space-x-3 flex items-center bg-slate-800 ">
-                                    <i class='bx bx-check-circle bx-sm'></i>
-                                    @if ($item['user_tujuan'] !== null)
-                                        <div class="">
-                                            <p class="text-lg">
-                                                @if ($item['status_disposisi'] == 1)
-                                                Didistribusikan kepada 
-                                                @elseif($item['status_disposisi'] == 2)
-                                                Didisposisikan kepada 
-                                                @elseif($item['status_disposisi'] == 3)
-                                                Diteruskan kepada 
-                                                @endif
-                                                {{ $item['user_tujuan']['name'] }}
-                                                @if ($item['user_tujuan']['jabatan'] !== null)
-                                                    - {{ $item['user_tujuan']['jabatan']['nama_jabatan'] }}
-                                                @endif
-                                            </p>
-                                            <p class="">Didisposisikan oleh {{ $item['user']['name'] }}
-                                                @if ($item['user']['jabatan'] !== null)
-                                                    - {{ $item['user']['jabatan']['nama_jabatan'] }}
-                                                @endif
-                                            </p>
-                                        </div>
-                                    @elseif ($item['status_disposisi'] == 5)
-                                        <div class="">
-                                            <p class="text-lg"> Surat Ditolak
-                                            </p>
-                                            <p class="">Ditolak oleh {{ $item['user']['name'] }}
-                                                @if ($item['user']['jabatan'] !== null)
-                                                    - {{ $item['user']['jabatan']['nama_jabatan'] }}
-                                                @endif
-                                            </p>
-                                        </div>
-                                    @elseif ($item['status_disposisi'] == 4)
-                                        <div class="">
-                                            <p class="text-lg"> Surat Ditandai Selesai
-                                            </p>
-                                            <p class="">Ditandai selesai oleh {{ $item['user']['name'] }}
-                                                @if ($item['user']['jabatan'] !== null)
-                                                    - {{ $item['user']['jabatan']['nama_jabatan'] }}
-                                                @endif
-                                            </p>
-                                        </div>
+                <div class="box-body mb-12">
+                <ul class="timeline list-none text-[0.813rem] text-defaulttextcolor">
+                    @foreach ($data as $item)
+                    <li>
+                        <div class="timeline-time text-end">
+                        <span class="date">{{ \Carbon\Carbon::parse($item['tgl_disposisi'])->translatedFormat('l') }}</span>
+                        <span class="time inline-block">{{ Carbon::parse($item['tgl_disposisi'])->translatedFormat('d F Y') }}</span>
+                        </div>
+                        <div class="timeline-icon">
+                        <a aria-label="anchor" href="javascript:void(0);"></a>
+                        </div>
+                        <div class="timeline-body">
+                            <div class="flex items-start timeline-main-content flex-wrap mt-0">
+                                <div class="avatar avatar-md online me-3 avatar-rounded md:mt-0 mt-6">
+                                    @if ($item['status_disposisi'] == 1)
+                                        <span class="avatar avatar-rounded bg-warning">
+                                            <i class="ri-mail-send-line text-[1rem] text-white"></i>
+                                        </span>
+                                    @elseif($item['status_disposisi'] == 2)
+                                        <span class="avatar avatar-rounded bg-success">
+                                            <i class="ri-mail-check-line text-[1rem] text-white"></i>
+                                        </span>
+                                    @elseif($item['status_disposisi'] == 3)
+                                        <span class="avatar avatar-rounded bg-secondary">
+                                            <i class="ri-mail-download-line  text-[1rem] text-white"></i>
+                                        </span>
+                                    @elseif($item['status_disposisi'] == 4)
+                                        <span class="avatar avatar-rounded bg-primary">
+                                            <i class="ri-inbox-archive-line text-[1rem] text-white"></i>
+                                        </span>
+                                    @elseif($item['status_disposisi'] == 5)
+                                        <span class="avatar avatar-rounded bg-danger">
+                                            <i class="ri-mail-close-line text-[1rem] text-white"></i>
+                                        </span>
                                     @endif
+
+                                </div>
+                                <div class="flex-grow">
+                                    <div class="flex items-center">
+                                        @if ($item['user_tujuan'] !== null)
+                                            <div class="sm:mt-0 mt-2">
+                                                <p class="mb-0 text-[.875rem] font-semibold">
+                                                    @if ($item['status_disposisi'] == 1)
+                                                    Didistribusikan kepada: 
+                                                    @elseif($item['status_disposisi'] == 2)
+                                                    Didisposisikan kepada: 
+                                                    @elseif($item['status_disposisi'] == 3)
+                                                    Diteruskan kepada: 
+                                                    @endif
+                                                    {{ $item['user_tujuan']['name'] }}</p>
+                                                    @if ($item['user_tujuan']['jabatan'] !== null)
+                                                        <p class="mb-0 text-[#6a6d71] dark:text-white/50">{{ $item['user_tujuan']['jabatan']['nama_jabatan'] }}</p>
+                                                    @endif
+                                                <p class="mb-0 text-[#8c9097] dark:text-white/50">Pengirim: {{ $item['user']['name'] }}
+                                                    @if ($item['user']['jabatan'] !== null)
+                                                        - {{ $item['user']['jabatan']['nama_jabatan'] }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                        @elseif ($item['status_disposisi'] == 5)
+                                            <div class="sm:mt-0 mt-2">
+                                                <p class="mb-0 text-[.875rem] font-semibold">Surat Ditolak
+                                                </p>
+                                                <p class="mb-0 text-[#8c9097] dark:text-white/50">Ditolak oleh: {{ $item['user']['name'] }}
+                                                    @if ($item['user']['jabatan'] !== null)
+                                                        - {{ $item['user']['jabatan']['nama_jabatan'] }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                        @elseif ($item['status_disposisi'] == 4)
+                                            <div class="sm:mt-0 mt-2">
+                                                <p class="mb-0 text-[.875rem] font-semibold"> Surat Ditandai Selesai
+                                                </p>
+                                                <p class="">Ditandai selesai oleh: {{ $item['user']['name'] }}
+                                                    @if ($item['user']['jabatan'] !== null)
+                                                        - {{ $item['user']['jabatan']['nama_jabatan'] }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    </li>
+                    @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
-    </div>
+     </div>
 @endsection
